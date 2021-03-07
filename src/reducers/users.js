39282c946bc648a3b3c6bc0a5bf421d0ec169a43
yushan <yushan@intervest.lk>
2021-03-07 18:54:15 +0530

    feat: SAVE_QUESTION action added for users reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -8,13 +8,22 @@ export default function users(state = {}, action) {
                 ...state,
                 ...action.users
             }
+        case SAVE_QUESTION:
+            const { author, id } = action.polls;
+            return {
+                ...state,
+                [author]: {
+                    ...state[author],
+                    questions: state[author].questions.concat([id])
+                }
+            }
         case SAVE_ANSWER:
             const { authedUser, qid } = action.answer;
             return {
                 ...state,
                 [authedUser]: {
-                    ...state.users[authedUser],
-                    questions: state.users[authedUser].questions.concat([qid])
+                    ...state[authedUser],
+                    questions: state[authedUser].questions.concat([qid])
                 }
             }
         default:
