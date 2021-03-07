2ae96656835d9b108b362f58943b0ff5ab623a8f
yushan <yushan@intervest.lk>
2021-03-07 19:17:03 +0530

    feat: authUser combine to other reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import polls from './polls';
 import users from './users';
+import authUser from './authUser';
 
 export default combineReducers({
     polls,
-    users
+    users,
+    authUser
 });
