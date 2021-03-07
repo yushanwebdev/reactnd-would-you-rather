282dd1061c5ac709c7cdb031b009828bf860d07c
yushan <yushan@intervest.lk>
2021-03-07 14:30:16 +0530

    feat: polls action creators added

--- src/actions/polls.js
+++ src/actions/polls.js
@@ -1,3 +1,50 @@
+import { getQuestions, saveQuestion, saveQuestionAnswer } from '../utils/api';
+
 export const GET_POLLS = 'GET_POLLS';
-export const SAVE_QUESTIONS = 'SAVE_QUESTIONS';
-export const SAVE_ANSWERS = 'SAVE_ANSWERS';
+export const SAVE_QUESTION = 'SAVE_QUESTIONS';
+export const SAVE_ANSWER = 'SAVE_ANSWERS';
+
+function receivePolls(polls) {
+    return {
+        type: GET_POLLS,
+        polls
+    }
+}
+
+export function handleReceivePolls() {
+    return (dispatch) => {
+        return getQuestions()
+            .then((polls) => dispatch(receivePolls(polls)))
+    }
+}
+
+function receiveQuestion(poll) {
+    return {
+        type: SAVE_QUESTION,
+        poll
+    }
+}
+
+export function handleSaveQuestion(question) {
+    return (dispatch) => {
+        return saveQuestion(question)
+            .then((poll) => receiveQuestion(poll))
+    }
+}
+
+function receiveAnswer(answer) {
+    return {
+        type: SAVE_ANSWER,
+        answer
+    }
+}
+
+export function handleSaveAnswer(answer) {
+    return (dispatch) => {
+        receiveAnswer(answer)
+        return saveQuestionAnswer(answer)
+            .catch(() => {
+                console.warn('Answer is not saved.')
+            })
+    }
+}
