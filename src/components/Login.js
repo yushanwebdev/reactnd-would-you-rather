a2b575f28ddb8cc63d1fcf0b4d37d914d34351a2
yushan <yushan@intervest.lk>
2021-03-07 22:35:05 +0530

    feat: Login skeleton created

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return(
+            <div>
+                Login
+            </div>
+        )
+    }
+}
+
+export default Login;
