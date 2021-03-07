46c5b92a49f64bc1acf8634409fd56919d9fd680
yushan <yushan@intervest.lk>
2021-03-07 18:09:44 +0530

    feat: users action creators created

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,1 +1,17 @@
+import { getUsers } from '../utils/api';
+
 export const GET_USERS = 'GET_USERS';
+
+function receiveUsers(users) {
+    return {
+        type: GET_USERS,
+        users
+    }
+}
+
+export function handleReceiveUsers() {
+    return (dispatch) => {
+        return getUsers()
+            .then((users) => receiveUsers(users))
+    }
+}
