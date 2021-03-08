9922a660274d614b2ab1af43e40c196e85829901
yushan <yushan@intervest.lk>
2021-03-08 21:44:41 +0530

    feat: UN_SET_AUTH_USER case added to the authUser reducer

--- src/reducers/authUser.js
+++ src/reducers/authUser.js
@@ -1,9 +1,11 @@
-import { SET_AUTH_USER } from '../actions/authUser';
+import { SET_AUTH_USER, UN_SET_AUTH_USER } from '../actions/authUser';
 
-export default function authUser(state = {}, action) {
+export default function authUser(state = null, action) {
     switch (action.type) {
         case SET_AUTH_USER:
             return action.id
+        case UN_SET_AUTH_USER:
+            return null
         default:
             return state
     }
