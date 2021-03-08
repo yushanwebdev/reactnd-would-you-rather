81f4736b6a015fe710370be8e92677690cff912e
yushan <yushan@intervest.lk>
2021-03-08 21:43:32 +0530

    feat: unSetAuthUser action creator added to the authUser actions

--- src/actions/authUser.js
+++ src/actions/authUser.js
@@ -1,8 +1,15 @@
 export const SET_AUTH_USER = 'SET_AUTH_USER';
+export const UN_SET_AUTH_USER = 'UN_SET_AUTH_USER';
 
 export function setAuthUser(id) {
     return {
         type: SET_AUTH_USER,
         id
+    }
+}
+
+export function unSetAuthUser() {
+    return {
+        type: UN_SET_AUTH_USER
     }
 }
