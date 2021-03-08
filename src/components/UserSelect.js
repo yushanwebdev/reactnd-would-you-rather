d531fe2ff68328f4422ae7338e2d70db98b9a070
yushan <yushan@intervest.lk>
2021-03-08 21:46:16 +0530

    feat: getAuthUser created to dispatch relevant actions on select onChange

--- src/components/UserSelect.js
+++ src/components/UserSelect.js
@@ -1,12 +1,23 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import UserItem from './UserItem';
+import { setAuthUser, unSetAuthUser } from '../actions/authUser';
 
 class UserSelect extends Component {
+    getAuthUser = (e) => {
+        const { dispatch } = this.props;
+        const val = e.target.value;
+
+        const action = val ? setAuthUser(val) : unSetAuthUser();
+
+        dispatch(action);
+    }
+
     render() {
         const { userIds } = this.props;
         return (
-            <select>
+            <select onChange={this.getAuthUser}>
+                <option value="">Select User</option>
                 {userIds.map((user) => (
                     <UserItem key={user} id={user} />
                 ))}
