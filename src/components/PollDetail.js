42ae3afe390aa56802f74d610f07c0cd02dd729f
yushan <yushan@intervest.lk>
2021-03-11 01:26:13 +0530

    feat: PollDetail skeleton created

---
+++ src/components/PollDetail.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class PollDetails extends Component {
+    render() {
+        return(
+            <div>
+                Poll Detail
+            </div>
+        )
+    }
+}
+
+export default PollDetails;
