f8719a6d5289700d55c059f51191bda24690031c
yushan <yushan@intervest.lk>
2021-03-11 12:03:57 +0530

    feat: PollAns skeleton created

---
+++ src/components/PollAns.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class PollAns extends Component {
+    render() {
+        return(
+            <div>
+                Poll Answer
+            </div>
+        )
+    }
+}
+
+export default PollAns;
