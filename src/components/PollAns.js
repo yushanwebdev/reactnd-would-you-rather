e921ceba61e20334142a924f74be6fa72bfda526
yushan <yushan@intervest.lk>
2021-03-12 23:19:44 +0530

    feat: PollAns component render with data

--- src/components/PollAns.js
+++ src/components/PollAns.js
@@ -1,13 +1,43 @@
 import { Component } from 'react';
+import { connect } from 'react-redux';
+import PollResult from './PollResult';
 
 class PollAns extends Component {
     render() {
-        return(
+        const { author, qid } = this.props;
+        return (
             <div>
-                Poll Answer
+                <div>
+                    <p>{author?.name}</p>
+                    <img src={`/profiles/${author?.avatarURL}`} width="50" height="50" />
+                </div>
+                <div>
+                    <p>Results:</p>
+                    <div>
+                        <ul>
+                            <li>
+                                <PollResult
+                                    qid={qid}
+                                    ansConst={process.env.REACT_APP_OPTION_ONE} />
+                            </li>
+                            <li>
+                                <PollResult
+                                    qid={qid}
+                                    ansConst={process.env.REACT_APP_OPTION_TWO} />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-export default PollAns;
+function mapStateToProps({ authUser, users, polls }, { qid }) {
+    const poll = polls[qid];
+    return {
+        author: users[poll?.author]
+    }
+}
+
+export default connect(mapStateToProps)(PollAns);
