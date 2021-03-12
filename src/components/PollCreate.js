d33381d4db047abe9df49f3d433f4b4340efb647
yushan <yushan@intervest.lk>
2021-03-13 00:09:06 +0530

    feat: PollCreate component updated with actions dispatching

--- src/components/PollCreate.js
+++ src/components/PollCreate.js
@@ -1,13 +1,58 @@
 import { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleSaveQuestion } from '../actions/polls';
 
 class PollCreate extends Component {
+    state = {
+        [process.env.REACT_APP_OPTION_ONE]: '',
+        [process.env.REACT_APP_OPTION_TWO]: ''
+    }
+
+    inputChange = (e) => {
+        const ele = e.target;
+        this.setState((prev) => ({
+            [ele.name]: ele.value
+        }));
+    }
+
+    saveQuestion = (e) => {
+        e.preventDefault();
+        const { dispatch, authUser } = this.props;
+        const { optionOne, optionTwo } = this.state;
+        dispatch(handleSaveQuestion({ 
+            optionOneText: optionOne,
+            optionTwoText: optionTwo,
+            author: authUser
+        }));
+    }
+
     render() {
+        const { optionOne, optionTwo } = this.state; 
         return(
             <div>
-                Poll Create
+                <form onSubmit={this.saveQuestion}>
+                    <input 
+                        type="text" 
+                        name={process.env.REACT_APP_OPTION_ONE} 
+                        value={optionOne} 
+                        onChange={this.inputChange} />
+                    <p>OR</p>
+                    <input 
+                        type="text" 
+                        name={process.env.REACT_APP_OPTION_TWO} 
+                        value={optionTwo} 
+                        onChange={this.inputChange} />
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
 }
 
-export default PollCreate;
+function mapStateToProps({ authUser }) {
+    return {
+        authUser
+    }
+}
+
+export default connect(mapStateToProps)(PollCreate);
