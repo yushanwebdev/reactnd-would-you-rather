e38b5b61bb8de1179a2c62f15943b0ba4a4bc6e7
yushan <yushan@intervest.lk>
2021-03-12 23:34:54 +0530

    feat: PollCreate skeleton created

---
+++ src/components/PollCreate.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class PollCreate extends Component {
+    render() {
+        return(
+            <div>
+                Poll Create
+            </div>
+        )
+    }
+}
+
+export default PollCreate;
