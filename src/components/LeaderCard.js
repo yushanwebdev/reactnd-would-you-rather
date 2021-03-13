e0b1b7c20d57b10921dae515e3ed914eb6fcb3c7
yushan <yushan@intervest.lk>
2021-03-13 20:45:34 +0530

    feat: LeaderCard component's skeleton created

---
+++ src/components/LeaderCard.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class LeaderCard extends Component {
+    render() {
+        return(
+            <div>
+                LeaderCard
+            </div>
+        )
+    }
+}
+
+export default LeaderCard;
