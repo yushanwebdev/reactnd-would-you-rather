31cffd3b1558524997005b3dcb727e1228e81fd4
yushan <yushan@intervest.lk>
2021-03-13 21:52:11 +0530

    feat: Develop UI of the LeaderCard component with data

--- src/components/LeaderCard.js
+++ src/components/LeaderCard.js
@@ -3,10 +3,25 @@ import { connect } from 'react-redux';
 
 class LeaderCard extends Component {
     render() {
-        console.log(this.props);
+        const { user } = this.props;
+        const created = Object.keys(user.questions).length;
+        const answered = Object.keys(user.answers).length;
         return(
             <div>
-                LeaderCard
+                <p>{user.name}</p>
+                <img src={`/profiles/${user.avatarURL}`} width="50" height="50"/>
+                <div>
+                    <p>Created</p>
+                    <p>{created}</p>
+                </div>
+                <div>
+                    <p>Answered</p>
+                    <p>{answered}</p>
+                </div>
+                <div>
+                    <p>Total</p>
+                    <p>{created + answered}</p>
+                </div>
             </div>
         )
     }
