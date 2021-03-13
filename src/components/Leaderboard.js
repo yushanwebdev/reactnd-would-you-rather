6446fdce15f29f0485b031a84f3d4c4795df1d04
yushan <yushan@intervest.lk>
2021-03-13 20:21:52 +0530

    feat: Leaderboard component's skeleton created

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Leaderboard extends Component {
+    render() {
+        return(
+            <div>
+                Leaderboard
+            </div>
+        )
+    }
+}
+
+export default Leaderboard;
