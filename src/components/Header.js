6118e257534d922723630331b42bdbbbbf6e8b42
yushan <yushan@intervest.lk>
2021-03-13 22:01:53 +0530

    feat: Add menu section to the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import { Component } from 'react';
 
 class Header extends Component {
     render() {
-        return(
+        return (
             <div>
-                Header
+                <div>
+                    <a href="#">Home</a>
+                    <a href="#">New Question</a>
+                    <a href="#">Leader Board</a>
+                </div>
             </div>
         )
     }
