3a91848e913bd86eee6360be01079d62629ece37
yushan <yushan@intervest.lk>
2021-03-14 00:25:37 +0530

    feat: routes created in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { handleReceivePolls } from './actions/polls';
 import { handleReceiveUsers } from './actions/users';
 import { setAuthUser } from './actions/authUser';
@@ -11,22 +12,28 @@ import Leaderboard from './components/Leaderboard';
 import Header from './components/Header';
 
 class App extends Component {
-  componentDidMount() {
-    const { dispatch } = this.props;
-
-    dispatch(handleReceiveUsers());
-    dispatch(setAuthUser('tylermcginnis'));
-    dispatch(handleReceivePolls());
-  }
-
   render() {
-    return(
-      <div>
-        <Header />
-        <Leaderboard />
-      </div>
+    const { authUser } = this.props;
+
+    return (
+      <Router>
+        <Fragment>
+          <Header />
+          <Route path="/login" component={Login} />
+          <Route path="/questions/:id" component={PollDetail} />
+          <Route path="/add" component={PollCreate} />
+          <Route path="/leaderboard" component={Leaderboard} />
+          <Route exact path="/" component={Dashboard} />
+        </Fragment>
+      </Router>
     )
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ authUser }) {
+  return {
+    authUser
+  }
+}
+
+export default connect(mapStateToProps)(App);
