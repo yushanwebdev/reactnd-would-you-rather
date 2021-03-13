6cbb189505542ff1a40ffda2dd5f2dff9b9be419
yushan <yushan@intervest.lk>
2021-03-13 20:43:09 +0530

    feat: users sorted in Leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -3,12 +3,26 @@ import { connect } from 'react-redux';
 
 class Leaderboard extends Component {
     render() {
+        console.log(this.props);
         return(
             <div>
-                Leaderboard
+                <div>
+                    Leaderboard
+                </div>
             </div>
         )
     }
 }
 
-export default Leaderboard;
+function mapStateToProps({ users }) {
+    const userIds = Object.keys(users);
+    const total = (id) => {
+        return Object.keys(users[id].answers).length + Object.keys(users[id].questions).length;
+    }
+    return {
+        board: userIds.sort((a, b) => total(b) - total(a))
+    }
+
+}
+
+export default connect(mapStateToProps)(Leaderboard);
