219d10a27c3d3b98a30bb405028015775c440b6b
yushan <yushan@intervest.lk>
2021-03-20 18:26:37 +0530

    feat: <Header /> convert to stateless functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,41 +4,39 @@ import { NavLink } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 import { unSetAuthUser } from '../actions/authUser';
 
-class Header extends Component {
-    logout = (e) => {
+const Header = (props) => {
+    const { authedUser, dispatch } = props;
+    const logout = (e) => {
         e.preventDefault();
-        this.props.dispatch(unSetAuthUser());
+        dispatch(unSetAuthUser());
     }
 
-    render() {
-        const { authedUser } = this.props;
-        return (
-            <Navbar bg="light" expand="lg" className="mb-5 header">
-                <NavLink className="navbar-brand" to="/">Would You Rather</NavLink>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/add">New Question</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/leaderboard">Leader Board</NavLink>
-                        </li>
-                    </Nav>
-                    <div>
-                        <span>Hello, {authedUser?.name}</span>
-                        <img 
-                            src={`/profiles/${authedUser?.avatarURL}`} 
-                            width="30" 
-                            height="30" 
-                            className="ml-3 mr-2"
-                            alt={authedUser?.name} />
-                        <button className="btn btn-primary" onClick={this.logout}>Logout</button>
-                    </div>
-                </Navbar.Collapse>
-            </Navbar>
-        )
-    }
+    return (
+        <Navbar bg="light" expand="lg" className="mb-5 header">
+            <NavLink className="navbar-brand" to="/">Would You Rather</NavLink>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/add">New Question</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/leaderboard">Leader Board</NavLink>
+                    </li>
+                </Nav>
+                <div>
+                    <span>Hello, {authedUser?.name}</span>
+                    <img
+                        src={`/profiles/${authedUser?.avatarURL}`}
+                        width="30"
+                        height="30"
+                        className="ml-3 mr-2"
+                        alt={authedUser?.name} />
+                    <button className="btn btn-primary" onClick={logout}>Logout</button>
+                </div>
+            </Navbar.Collapse>
+        </Navbar>
+    )
 }
 
 function mapStateToProps({ authUser, users }) {
