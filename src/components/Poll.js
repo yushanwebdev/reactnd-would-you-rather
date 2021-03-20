5892be8b8d6ec7067a6262f92df4a650d108025c
yushan <yushan@intervest.lk>
2021-03-20 18:40:23 +0530

    feat: <Poll /> convert to a stateless functional component

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,23 +1,21 @@
-import { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-class Poll extends Component {
-    render() {
-        const { author, poll } = this.props;
-        return (
-            <Card className="poll">
-                <Link to={`/questions/${poll.id}`} className="d-flex">
-                    <Card.Img variant="left" src={`/profiles/${author.avatarURL}`} width="90" height="90" />
-                    <Card.Body>
-                        <p>{author.name}</p>
-                        <p>...{poll.optionTwo.text}</p>
-                    </Card.Body>
-                </Link>
-            </Card>
-        )
-    }
+const Poll = (props) => {
+    const { author, poll } = props;
+
+    return (
+        <Card className="poll">
+            <Link to={`/questions/${poll.id}`} className="d-flex">
+                <Card.Img variant="left" src={`/profiles/${author.avatarURL}`} width="90" height="90" />
+                <Card.Body>
+                    <p>{author.name}</p>
+                    <p>...{poll.optionTwo.text}</p>
+                </Card.Body>
+            </Link>
+        </Card>
+    )
 }
 
 function mapStateToProps({ polls, users }, { id }) {
