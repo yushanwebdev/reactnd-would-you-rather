a26c2818b9a40f0126162f5490d41ed4183e6133
yushan <yushan@intervest.lk>
2021-03-20 18:42:20 +0530

    feat: <PollAns /> convert to a stateless functional component

--- src/components/PollAns.js
+++ src/components/PollAns.js
@@ -3,37 +3,36 @@ import { Card } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import PollResult from './PollResult';
 
-class PollAns extends Component {
-    render() {
-        const { author, qid } = this.props;
-        return (
-            <Card className="poll-detail-info">
-                <Card.Header as="h6">Asked by {author?.name}</Card.Header>
-                <Card.Body className="d-flex">
+const PollAns = (props) => {
+    const { author, qid } = props;
+
+    return (
+        <Card className="poll-detail-info">
+            <Card.Header as="h6">Asked by {author?.name}</Card.Header>
+            <Card.Body className="d-flex">
+                <div>
+                    <img src={`/profiles/${author?.avatarURL}`} width="100" height="100" alt={author?.name} />
+                </div>
+                <div className="pl-4">
+                    <p>Results:</p>
                     <div>
-                        <img src={`/profiles/${author?.avatarURL}`} width="100" height="100" alt={author?.name} />
+                        <ul>
+                            <li className="mb-3">
+                                <PollResult
+                                    qid={qid}
+                                    ansConst={process.env.REACT_APP_OPTION_ONE} />
+                            </li>
+                            <li>
+                                <PollResult
+                                    qid={qid}
+                                    ansConst={process.env.REACT_APP_OPTION_TWO} />
+                            </li>
+                        </ul>
                     </div>
-                    <div className="pl-4">
-                        <p>Results:</p>
-                        <div>
-                            <ul>
-                                <li className="mb-3">
-                                    <PollResult
-                                        qid={qid}
-                                        ansConst={process.env.REACT_APP_OPTION_ONE} />
-                                </li>
-                                <li>
-                                    <PollResult
-                                        qid={qid}
-                                        ansConst={process.env.REACT_APP_OPTION_TWO} />
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </Card.Body>
-            </Card>
-        )
-    }
+                </div>
+            </Card.Body>
+        </Card>
+    )
 }
 
 function mapStateToProps({ authUser, users, polls }, { qid }) {
