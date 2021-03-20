595c3adbe10ea780d2bebdda459291be18a123f0
yushan <yushan@intervest.lk>
2021-03-20 12:54:18 +0530

    feat: <Tabs />, <Tab />, <PollList /> created

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,48 +1,37 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
-import Poll from './Poll';
+import { Tabs, Tab } from 'react-bootstrap';
+import PollList from './PollList';
 
 class Dashboard extends Component {
     render() {
-        const { ans, unAns } = this.props;
-        return(
-            <div>
-                <div>
-                    <h2>Unanswered</h2>
-                    <ul>
-                        {unAns.map((item) => (
-                            <li key={item}>
-                                <Poll id={item} />
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div>
-                    <h2>Answered</h2>
-                    <ul>
-                        {ans.map((item) => (
-                            <li key={item}>
-                                <Poll id={item} />
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
+        const { unAns, ans } = this.props;
+        const unAnsTitle = "Unanswered";
+        const ansTitle = "Answered";
+        return (
+            <Tabs id="dashboard">
+                <Tab eventKey="unanswer" title={unAnsTitle}>
+                    <PollList list={unAns} />
+                </Tab>
+                <Tab eventKey="answer" title={ansTitle}>
+                    <PollList list={ans} />
+                </Tab>
+            </Tabs>
         )
     }
 }
 
 function mapStateToProps({ users, authUser, polls }) {
     const pollIds = Object.keys(polls);
-    const sortedPollIds = pollIds.length 
-                            ? pollIds
-                                .sort((a, b) => polls[b].timestamp - polls[a].timestamp) 
-                            : [];
-    
+    const sortedPollIds = pollIds.length
+        ? pollIds
+            .sort((a, b) => polls[b].timestamp - polls[a].timestamp)
+        : [];
+
     const ans = users[authUser] && pollIds.length
-                ? Object.keys(users[authUser].answers)
-                        .sort((a, b) => polls[b].timestamp - polls[a].timestamp) 
-                : [];
+        ? Object.keys(users[authUser].answers)
+            .sort((a, b) => polls[b].timestamp - polls[a].timestamp)
+        : [];
 
     const unAns = sortedPollIds.filter(poll => (
         !ans.includes(poll)
