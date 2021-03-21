4673eb4dc7d156c83ac7bc96dd439d8382370fb4
yushan <yushan@intervest.lk>
2021-03-21 13:47:34 +0530

    feat: Update the content in <NotFound />

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 const NotFound = () => {
     return(
         <Container>
-            <h1>Request Page is not Found...</h1>
+            <h1>Requested Page is not Found...</h1>
         </Container>
     )
 }
