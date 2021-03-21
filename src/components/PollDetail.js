8362e8941eb4ea4ca4971d9a13a83990765428ad
yushan <yushan@intervest.lk>
2021-03-21 12:41:58 +0530

    fix: Fix to show 404 page only when question id not exist (This issue triggers only in initial login)

--- src/components/PollDetail.js
+++ src/components/PollDetail.js
@@ -5,9 +5,9 @@ import PollAns from './PollAns';
 import PollQ from './PollQ';
 
 const PollDetail = (props) => {
-    const { isAns, isExist, qid } = props;
+    const { isAns, isPolls, isExist, qid } = props;
 
-    if(!isExist) {
+    if(isPolls && !isExist) {
         return <Redirect to="/404" />
     }
 
@@ -24,6 +24,7 @@ function mapStateToProps({ authUser, users, polls }, props) {
     const qid = props.match.params.id;
     return {
         isAns: users[authUser]?.answers[qid] === undefined,
+        isPolls: Object.keys(polls).length,
         isExist: polls[qid], 
         qid
     }
